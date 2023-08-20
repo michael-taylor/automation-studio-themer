81d4743bcba4741e459b67c245b72e86f3eb5e80
Michael R. Taylor <michael@atomic.city>
2023-08-20 00:17:22 -0500

    Added Clikt skeleton class

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,19 @@
-fun main() {
-    println("Hello, Kotlin/Native!")
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.ProgramResult
+import com.github.ajalt.clikt.parameters.options.eagerOption
+
+class Themer : CliktCommand() {
+    init {
+        eagerOption("--list") {
+            // List themes here
+            println("Listing...")
+            throw ProgramResult(0)
+        }
+    }
+
+    override fun run() {
+        println("Running")
+    }
 }
+
+fun main(args: Array<String>) = Themer().main(args)
