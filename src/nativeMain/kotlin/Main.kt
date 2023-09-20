d720800c298f610855ceb90f24720749bed57f91
Michael R. Taylor <michael@atomic.city>
2023-09-20 01:13:08 -0500

    Added command-line parameter handling

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,18 +1,39 @@
+import automationstudio.EditorSettings
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.ProgramResult
+import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.eagerOption
+import themes.DraculaTheme
+
+val themes = listOf(
+    DraculaTheme(),
+)
 
 class Themer : CliktCommand() {
     init {
         eagerOption("--list") {
             // List themes here
-            println("Listing...")
+            for (t in themes) {
+                println("- ${t.name}")
+            }
             throw ProgramResult(0)
         }
     }
 
+    val theme by argument(help = "Name of theme")
+
     override fun run() {
-        println("Running")
+        val settings = EditorSettings()
+        val t = themes.find { it.name.equals(theme, ignoreCase = true) }
+
+        if (t == null)
+        {
+            println("ERROR: Theme $theme not found")
+            throw ProgramResult(-1)
+        }
+
+        settings.applyTheme(t)
+        println(settings.toXml())
     }
 }
 
