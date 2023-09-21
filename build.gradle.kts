abb917152962bf4f2c8adbf46391486ee468553d
Michael R. Taylor <michael@atomic.city>
2023-09-21 00:32:08 -0500

    Updated Gradle build script to only build for Windows

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,17 +11,8 @@ repositories {
 }
 
 kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+    // This project only supports Windows (Automation Studio itself is Windows-only)
+    val nativeTarget = mingwX64("native")
 
     nativeTarget.apply {
         binaries {
